a728062f978ffce77f0db2e4a9c8c13d1a40b21b
OpticRish <raj15rishabh@outlook.com>
2026-08-08 14:48:01 +0530

    initial commit

---
+++ src/S1W3/TrafficSignalStreakAnalyzer.java
@@ -0,0 +1,48 @@
+package S1W3;
+
+public class TrafficSignalStreakAnalyzer {
+
+    static void findLongestStreak(String signalLog) {
+
+        if (signalLog.isEmpty()) {
+            System.out.println("Signal log is empty");
+            return;
+        }
+
+        int currentStreak = 1;
+        int longestStreak = 1;
+
+        char currentColor = signalLog.charAt(0);
+        char longestColor = signalLog.charAt(0);
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+
+                currentStreak++;
+
+            } else {
+
+                currentStreak = 1;
+            }
+
+            if (currentStreak > longestStreak) {
+
+                longestStreak = currentStreak;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println(
+                "Longest Streak: '" + longestColor +
+                        "' repeated " + longestStreak + " times"
+        );
+    }
+
+    public static void main(String[] args) {
+
+        findLongestStreak("RRGGGYRR");
+
+        findLongestStreak("RRRRYYGG");
+    }
+}
